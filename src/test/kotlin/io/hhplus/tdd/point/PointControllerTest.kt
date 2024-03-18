90aa36f3bd8d0f252f737fc05cb56beabb73447e
Wanni <wannidev0928@gmail.com>
2024-03-18 22:38:15 +0900

    Test: 기본 기능을 검증하기위한 포인트 충전 테스트코드 작성

--- src/test/kotlin/io/hhplus/tdd/point/PointControllerTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointControllerTest.kt
@@ -33,5 +33,14 @@ class PointControllerTest {
         assertTrue(point.updateMillis > 0)
     }
 
+    @Test
+    @DisplayName("포인트 충전 시, 해당 유저의 id에 point를 충전한 point만큼 더하고, updateMillis를 현재 시간으로 설정한 UserPoint 객체를 반환합니다.")
+    fun charge() {
+        val point = controller.charge(1, 10000)
+        assertEquals(point.id, 1)
+        assertEquals(point.point, 10000)
+        assertTrue(point.updateMillis > 0)
+        println(controller.point(1))
+    }
 
 }
