fd781f96d31c9f01d904c0d42600aec910a0bc02
Wanni <wannidev0928@gmail.com>
2024-03-21 00:00:41 +0900

    Feat: 기존 충전, 사용 기능에 user ID 별로 락을 관리하여, 사용자가 충전과 사용을 하면서 발생하는 경합을 관리하도록 보완

--- src/main/kotlin/io/hhplus/tdd/point/PointController.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointController.kt
@@ -5,6 +5,10 @@ import io.hhplus.tdd.database.UserPointTable
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.locks.ReentrantLock
+import java.util.function.Supplier
 
 @RestController
 @RequestMapping("/point")
@@ -13,7 +17,8 @@ class PointController(
     private val pointHistoryTable: PointHistoryTable,
 ) {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
-
+    private val locks = ConcurrentHashMap<Long, ReentrantLock>()
+    private val executorService = Executors.newFixedThreadPool(10)
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
@@ -42,6 +47,22 @@ class PointController(
         @PathVariable id: Long,
         @RequestBody amount: Long,
     ): UserPoint {
+        val lock = locks.computeIfAbsent(id) { ReentrantLock() }
+        if (lock.tryLock()) {
+            try {
+                return chargePoint(id, amount)
+            } finally {
+                lock.unlock()
+            }
+        } else {
+            executorService.submit {
+                chargePoint(id, amount)
+            }
+            return userPointTable.selectById(id)
+        }
+    }
+
+    private fun chargePoint(id: Long, amount: Long): UserPoint {
         pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis())
         return userPointTable.insertOrUpdate(id, amount)
     }
@@ -54,9 +75,24 @@ class PointController(
         @PathVariable id: Long,
         @RequestBody amount: Long,
     ): UserPoint {
+        val lock = locks.computeIfAbsent(id) { ReentrantLock() }
+        if (lock.tryLock()) {
+            try {
+                return usePoint(id, amount)
+            } finally {
+                lock.unlock()
+            }
+        } else {
+            executorService.submit {
+                usePoint(id, amount)
+            }
+            return userPointTable.selectById(id)
+        }
+    }
+
+    private fun usePoint(id: Long, amount: Long): UserPoint {
         val userPoint = userPointTable.selectById(id)
         if (userPoint.point < amount) {
-            logger.error("$id 사용자의 포인트가 부족합니다.")
             throw IllegalArgumentException("포인트가 부족합니다.")
         }
         pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis())
